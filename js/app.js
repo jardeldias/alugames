47ef7acf3e1b493828ce7c0e6398a10a7089c7e6
Jardel Dias <jardel.g.dias@gmail.com>
2023-11-15 13:40:20 -0300

    Simulação de aluguel de jogos de tabuleiro.

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nome = gameClicado.querySelector('.dashboard__item__name');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+    }
+
+    if (botao.classList.contains('dashboard__item__button--return')) {
+        botao.classList.remove('dashboard__item__button--return');
+        botao.innerHTML = "Alugar";
+    } else {
+        botao.classList.add('dashboard__item__button--return');
+        botao.innerHTML = 'Devolver';
+    }
+}
